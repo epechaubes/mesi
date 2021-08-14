571bc0d7e1109da720add6ae0d040435063fb82a
emerick.pechaubes@ipilyon.net <Moi14051992>
2021-08-14 22:09:32 +0200

    Signup create user ok

--- mesi_project/login/views.py
+++ mesi_project/login/views.py
@@ -10,3 +10,20 @@ template = 'login/signup.html'
 
 def index(request):
     return HttpResponse("Index page")
+
+def signup(request):
+    return render(request, template)
+
+def signupSubmit(request):
+    if not request.method == 'POST':
+        form = SignUpForm()
+        context = {'form': form}
+        return render(request, template, context)
+
+    form = SignUpForm(request.POST)
+
+    if not form.is_valid():
+        return render(request, template)
+
+    form.save()
+    return redirect('index')
