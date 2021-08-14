067f0c9d27b06b6e22af567c7d4a46328eb3177b
emerick.pechaubes@ipilyon.net <Moi14051992>
2021-08-14 22:09:20 +0200

    Signup create user ok

--- mesi_project/login/urls.py
+++ mesi_project/login/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('signupSubmit/', views.signupSubmit, name='signupSubmit'),
+    path('signup/', views.signup, name='signup'),
     path('', views.index, name='index'),
 ]
--- mesi_project/login/views.py
+++ mesi_project/login/views.py
@@ -1,4 +1,12 @@
 from django.http import HttpResponse
+from django.shortcuts import redirect, render
+from django.contrib import messages
+from .forms import SignUpForm
+import logging
+
+
+logger = logging.getLogger(__name__)
+template = 'login/signup.html'
 
 def index(request):
     return HttpResponse("Index page")
