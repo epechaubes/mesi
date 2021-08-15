7d2ef7f112d82ab84ece9a6e5eb67464cc004359
emerick.pechaubes@ipilyon.net <Moi14051992>
2021-08-15 14:47:46 +0200

    Add HeroStory model and makemigrations

--- mesi_project/board/models.py
+++ mesi_project/board/models.py
@@ -1,2 +1,8 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
+
+class HeroStory(models.Model):
+    text = models.CharField(max_length=1000)
+    date = models.DateTimeField(auto_now=True)
+    author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
